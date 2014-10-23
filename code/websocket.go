5fb936186ca64667f4da8dc638dbde571988d717
Liana Lo <liana.lo@prezi.com>
2014-10-23 16:13:44 +0200

    websocket comments

--- code/websocket.go
+++ code/websocket.go
@@ -1,13 +1,20 @@
 // JavaScript to place into browser console to test server code. 
 
 /* 
+Construct a websocket connecting to localhost:4000
 var sock = new WebSocket("ws://localhost:4000/");
+
+message handler
+every message received will be logged in the JS console
 sock.onmessage = function(m) { console.log("Received:", m.data); }
+
+send the message hello
 sock.send("Hello!\n")
 */
 
 package code
 
+// Go's websocket package piggybacks off the http package.
 import (
 	"code.google.com/p/go.net/websocket"
 	"fmt"
@@ -16,15 +23,20 @@ import (
 
 func Websocket() {
 	http.Handle("/", websocket.Handler(websocket_handler)) // register websocket handler
-	http.ListenAndServer("localhost:4000", nil)
+	http.ListenAndServe("localhost:4000", nil)
 }
 
 // 25:21
-// bidirectional communication channel
+
+// takes in a single websocket connection
+// can read and write to websocket connection
+// bidirectional communication channel with JS running in the browser
+// when the handler returns, the websocket connection shuts down
 func websocket_handler(c *websocket.Conn) {
 	var s string
-	fmt.Fscan(c, &s)
-	fmt.Println("Received:", s)
-	fmt.Fprint(c, "How do you do?")
+	fmt.Fscan(c, &s) // read a \n newline separate string
+	fmt.Println("Received:", s) // prints received string to stdoutput
+	fmt.Fprint(c, "How do you do?") // writes response to websocket connection -- appears in JS console
+	// websocket closes
 }
 
--- main.go
+++ main.go
@@ -3,5 +3,6 @@ package main
 import "github.com/lilo/http_networking/code"
 
 func main() {
-    code.Hello()
+    // code.Hello()
+    code.Websocket()
 }
